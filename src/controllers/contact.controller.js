70042135f9dd99c7f7cd224c36e994ee5f66e574
Nguyễn Quốc Bảo <baonguyengau14@gmai.com>
2022-09-17 15:55:36 +0700

    Install express and show a welcome message

---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,31 @@
+ 
+exports.create = (req, res) => {
+	return res.send({ message: 'create handLer' });
+};
+
+exports.findALL = (req, res) => {
+	return res.send({ message: 'findALL handLer' });
+};
+
+exports.findOne = (req, res) => {
+	return res.send({ message: 'findOne handler' });
+
+};
+
+exports.Update = (req, nes) => {
+	return res.send({ message: 'update handLer' });
+
+};
+
+exports.deLete = (req, res) => {
+return res.send({ message: 'deLete handLer' });
+};
+
+exports.deLeteALL = (req, nes) => {
+	return res.send({ message: 'deLeteALL handLen' });
+
+};
+
+exports.findAtLFavorite = (req, rnes) => {
+	return res.send({ message: 'findAtLFavorite handLen' });
+};
